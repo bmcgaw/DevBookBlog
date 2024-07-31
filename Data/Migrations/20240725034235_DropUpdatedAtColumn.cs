498720b0062160abde2ebd1656eae4d46e4cc254
Brian McGaw <bmcgaw91@gmail.com>
2024-07-30 20:58:39 -0500

    Remove updated at column

---
+++ Data/Migrations/20240725034235_DropUpdatedAtColumn.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevBook.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropUpdatedAtColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Posts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+           name: "UpdatedAt",
+           table: "Posts",
+           nullable: true);
+        }
+    }
+}
+
+
+
+
